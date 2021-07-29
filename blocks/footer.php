7b34ae2a7f6f3673a0547230a671f92a839e94e0
Alex <87208475+PuSh0chek@users.noreply.github.com>
2021-07-29 17:00:49 +0300

    Add files via upload

---
+++ blocks/footer.php
@@ -0,0 +1,25 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row">
+      <div class="col-12 col-md">
+        <img class="mb-2" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+        <small class="d-block mb-3 text-muted">©2019-2020.При использовании сайта ссылка обязательна</small>
+      </div>
+      <div class="col-6 col-md">
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Новости сайта в формате RSS</a></li>
+          <li><a class="text-muted" href="#">Футбольный клуб</a></li>
+          <li><a class="text-muted" href="#">Разработчик сайта: A.V.G.</a></li>
+          <li><a class="text-muted" href="#">Генеральный партнер: CONSTELL GROUP</a></li>
+          <li><a class="text-muted" href="#">Все права защищены.</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Дизайн и технологическая поддержка A.V.G.</a></li>
+          <li><a class="text-muted" href="#">г.Ростов на дону, ул.Свободы 1</a></li>
+          <li><a class="text-muted" href="#">Телефон: +7-906-427-64-67</a></li>
+          <li><a class="text-muted" href="#">Почта: ticket@fc-footboll.ru</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
---
+++ blocks/header.php
@@ -0,0 +1,14 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm background">
+  <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.php">Футбольный клуб Штутгард</a></h5>
+  <img src="awards_img1.png" class="awards">
+  <img src="awards_img2.png" class="awards">
+  <img src="awards_img3.png" class="awards">
+  <img src="awards_img4(1).png" class="awards">
+    <nav class="my-2 my-md-0 mr-md-3">
+      <a class="p-2 text-dark" href="news.php">Новости</a>
+      <a class="p-2 text-dark" href="match.php">История клуба</a>
+      <a class="p-2 text-dark" href="about.php">Фото</a>
+      <a class="p-2 text-dark" href="#"></a>
+    </nav>
+  <a class="btn btn-outline-primary" href="/reg.php">Войти/Зарегистрироваться</a>
+</div>
